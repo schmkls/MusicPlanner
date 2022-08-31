ae141705af235868e1bfd438554cbda70581c6cd
schmkls <mickels.martin@gmail.com>
2022-08-31 18:33:59 +0200

    smooth skipping

--- frontend/src/functionality/spotifyControl.js
+++ frontend/src/functionality/spotifyControl.js
@@ -17,7 +17,7 @@ const spotifyControl = () => {
             const accessToken = accessor.getSpotifyAccessToken();
 
             const putUrl = `https://api.spotify.com/v1/me/player/volume?volume_percent=${volume}`;
-            axios.put(putUrl, null,{ headers: { Authorization: `Bearer ${accessToken}`} })
+            axios.put(putUrl, null, { headers: { Authorization: `Bearer ${accessToken}`} })
             .then((response) => {
                 if (response.status < 200 || response.status > 299) {
                     console.log("control volume bad response");
@@ -54,10 +54,23 @@ const spotifyControl = () => {
 
     const repeat = async(func, times) => {
         await func();
-        times && --times && repeat(func, times);
+        times && --times && await repeat(func, times);
+        return new Promise((res) => res("Repeat finished"));
     }
 
-    
+    const skipTrack = async() => {
+        return new Promise((res, rej) => {
+            const accessToken = accessor.getSpotifyAccessToken();
+            const postUrl = `https://api.spotify.com/v1/me/player/next`;
+            axios.post(postUrl, null, { headers: { Authorization: `Bearer ${accessToken}`} })
+            .then((response) => {
+                if (response.status < 200 || response.status > 299) return rej("Skip track bad resonse");
+                console.log("Skipped track!");
+                res("Skipped track");
+            })
+            .catch((err) => {return rej("Skip track error", err)})
+        });
+    }
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -77,12 +90,11 @@ const spotifyControl = () => {
             let currVolume = originalVolume;
             //hämta nuvarande volym
 
-            repeat(async() => {
+            await repeat(async() => {
                 console.log("controlling volume to: " + Math.round(currVolume - (originalVolume / nTimes)));
                 await controlVolume(Math.round(currVolume - (originalVolume / nTimes)));
                 currVolume = currVolume - (originalVolume / nTimes);
                 await sleep(500);
-                console.log("should have lowered volume");
             }, nTimes)
 
 
@@ -92,7 +104,27 @@ const spotifyControl = () => {
 
 
     const slowlyHigherVolume = async() => {
+        let nTimes = 8;
 
+        return new Promise(async(res, rej) => {
+            let originalVolume = await getCurrVolume()
+            .catch((err) => {
+                return (rej("Could not slowly higher volume"));
+            });
+
+            let currVolume = originalVolume;
+            //hämta nuvarande volym
+
+            await repeat(async() => {
+                console.log("controlling volume to: " + Math.round(currVolume + (100 / nTimes)));
+                await controlVolume(Math.round(currVolume + (100 / nTimes)));
+                currVolume = currVolume + (100 / nTimes);
+                await sleep(500);
+            }, nTimes)
+
+
+            return res("Volume lowered");
+        });
     }
 
 
@@ -101,11 +133,18 @@ const spotifyControl = () => {
         
         await slowlyLowerVolume()
         .catch((err) => {
-            console.log("Could not smoothskip");
+            console.log("Could not smoothskip ", err);
         })
 
-        //byt låt
-        //höj på samma sätt
+        await skipTrack()
+        .catch((err) => {
+            console.log("Could not smoothskip ", err);
+        })
+
+        await slowlyHigherVolume()
+        .catch((err) => {
+            console.log("Could not smoothskip ", err);
+        })
     }
 
 
